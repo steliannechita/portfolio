8ef36d45aaad562ef3c2dae7b147215d8779f846
stelian nechita <steliannechita2013@gmail.com>
2019-08-14 20:25:28 +0100

    Updates

---
+++ precache-manifest.7f4fe0297f3360c14fd6b3c710a59395.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966729cfae9dc6bc3b71fea5d37186f8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1aa9e5242ff2519895f6",
+    "url": "/portfolio/static/css/main.70fc72a8.chunk.css"
+  },
+  {
+    "revision": "17afe2203fa0290ca28c",
+    "url": "/portfolio/static/js/2.7d6b5f10.chunk.js"
+  },
+  {
+    "revision": "1aa9e5242ff2519895f6",
+    "url": "/portfolio/static/js/main.4463dce9.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f6a5f64d53a7f1e88c1830e882d95ae6",
+    "url": "/portfolio/static/media/Python.f6a5f64d.png"
+  },
+  {
+    "revision": "de2f626b57f914cec1da22a7c251b9dc",
+    "url": "/portfolio/static/media/css3.de2f626b.png"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "e0f850ead5e095cdc46a53905d777946",
+    "url": "/portfolio/static/media/jQuery.e0f850ea.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "41cb99a6f80de51ec630fad5619d9459",
+    "url": "/portfolio/static/media/node.41cb99a6.png"
+  },
+  {
+    "revision": "2962cebb8d7d725250b39d2be0eedf8b",
+    "url": "/portfolio/static/media/profile_pic.2962cebb.jpg"
+  },
+  {
+    "revision": "84e2b47fe58d07290ffd80b072a76a20",
+    "url": "/portfolio/static/media/project1.84e2b47f.png"
+  },
+  {
+    "revision": "f05fb721012291fc40a78666150d3093",
+    "url": "/portfolio/static/media/project2.f05fb721.png"
+  },
+  {
+    "revision": "062b7fa388d07befadcb3e3cba0098d5",
+    "url": "/portfolio/static/media/project3.062b7fa3.png"
+  },
+  {
+    "revision": "6d6aa0ac70bfef8ee9ff0699010e4a73",
+    "url": "/portfolio/static/media/react.6d6aa0ac.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "dc737c10bc1de3a16e43f6f981c18dbe",
+    "url": "/portfolio/static/media/section-image-SKILLS.dc737c10.jpg"
+  },
+  {
+    "revision": "1be1d41dfb382d2985481f43fb3b8a47",
+    "url": "/portfolio/static/media/section-image-bio.1be1d41d.jpg"
+  },
+  {
+    "revision": "96d5f8b1d7b07b4adcaeaaba1721e3d7",
+    "url": "/portfolio/static/media/section-image-contact.96d5f8b1.jpg"
+  },
+  {
+    "revision": "074e7ce1cf3557f8aaef401c622c7d87",
+    "url": "/portfolio/static/media/section-image-projects.074e7ce1.jpg"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "/portfolio/static/media/twitter_icon.512536cb.png"
+  }
+]);
