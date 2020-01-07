aafa326bcc15af4b6907a7e06368c1031a90cdd8
Stelian <steliannechita2013@gmail.com>
2020-01-07 16:04:47 +0000

    project 4

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,27 @@
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
+import project4 from "../assets/project4.png";
 
 const PROJECTS = [
+  {
+    id: 3,
+    title: "Ravenous",
+    description:
+      "Ravenous is a web application that acts as a Yelp.com clone, where users can search for their favorite restaurants or type of food within a specific area, filtering results using available sort options.",
+    link: "https://steliannechita.github.io/ravenous/",
+    image: project3,
+    sourceCode: "https://github.com/steliannechita/ravenous"
+  },
+  {
+    id: 4,
+    title: "Hangman game",
+    description:
+      "An interactive version of the Hangman game built with JavaScript Vanilla and Bootstrap.",
+    link: "https://steliannechita.github.io/Hangman-Vanilla-JS/",
+    image: project4,
+    sourceCode: "https://github.com/steliannechita/Hangman-Vanilla-JS"
+  },
   {
     id: 1,
     title: "Random Quote Generator",
@@ -21,15 +40,7 @@ const PROJECTS = [
     image: project2,
     sourceCode: "https://github.com/steliannechita/meme-generator"
   },
-  {
-    id: 3,
-    title: "Ravenous",
-    description:
-      "Ravenous is a web application that acts as a Yelp.com clone, where users can search for their favorite restaurants or type of food within a specific area, filtering results using available sort options.",
-    link: "https://steliannechita.github.io/ravenous/",
-    image: project3,
-    sourceCode: "https://github.com/steliannechita/ravenous"
-  }
+  
 ];
 
 export default PROJECTS;
